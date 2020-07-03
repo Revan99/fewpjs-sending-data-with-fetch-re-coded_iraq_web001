fe95e52c17eb7777aed34a6feb8e7b5438e04d59
Revan Sarbast <revansarbast8@gmail.com>
2020-07-03 08:51:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Add your code here
+let configObj = {
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+function sumitData(name, email){
+  fetch('http://localhost:3000/users', {users:name,email:email}).then().then().catch(err=>console.log(err))
+}
