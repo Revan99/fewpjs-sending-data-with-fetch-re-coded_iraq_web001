c356be513aca0dd50fbea7c9fa4c18515bfa9be6
Revan Sarbast <revansarbast8@gmail.com>
2020-07-03 08:53:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-// Add your code here
-let configObj = {
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify(formData)
-};
-function sumitData(name, email){
-  fetch('http://localhost:3000/users', {users:name,email:email}).then().then().catch(err=>console.log(err))
+function submitData( n, e ) {
+  return fetch( 'http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( {
+        n,
+        e
+      } )
+    } )
+    .then( function ( res ) {
+      return res.json()
+    } )
+    .then( function ( obj ) {
+      document.body.innerHTML = obj[ "id" ]
+    } )
+   
 }
